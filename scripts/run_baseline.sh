632df9c310c55785002a3bd76ca0adf4351d8ab4
Tiagoblima <tiago.blima@ufrpe.br>
2023-12-04 21:36:33 -0300

    fixing bugs by removing evaluating options

--- scripts/run_baseline.sh
+++ scripts/run_baseline.sh
@@ -1,7 +1,10 @@
-export $(grep -v '^#' /content/question-generation-ptbr/scripts/.env | xargs)
+export $(grep -v '^#' /scripts/.env | xargs)
 WANDB_PROJECT=question-generation-ptbr
 
-python /content/question-generation-ptbr/src/run_seq2seq_qg.py \
+echo $PWD
+pip install -r requirements.txt
+
+python /src/run_seq2seq_qg.py \
   --model_name_or_path unicamp-dl/ptt5-small-t5-vocab \
   --dataset_name tiagoblima/qg_squad_v1_pt \
   --context_column paragraph \
@@ -18,7 +21,4 @@ python /content/question-generation-ptbr/src/run_seq2seq_qg.py \
   --report_to "wandb" \
   --run_name "exp-baseline" \
   --push_to_hub_token $HUGGINGFACE_TOKEN \
-  --output_dir /tmp/debug_t5-small_squad/ \
-  --save_total_limit 1 \
-  --load_best_model_at_end \
-  --evaluation_strategy "steps"
+  --output_dir /tmp/debug_t5-small_squad/
