6cdc335b27ce636f35ad557cc4ca115e50b60af0
Tiagoblima <tiago.blima@ufrpe.br>
2023-12-29 19:30:17 -0300

    testing peft

--- src/run_seq2seq_qg.py
+++ src/run_seq2seq_qg.py
@@ -744,8 +744,9 @@ def main():
     peft_model = get_peft_model(model, lora_config)
 
     # Initialize our Trainer
+    print(train_dataset)
     trainer = QuestionAnsweringSeq2SeqTrainer(
-        model=model,
+        model=peft_model,
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
