9735882d28f1d7a2784d3a13e29c406da3789254
Tiagoblima <tiago.blima@ufrpe.br>
2024-01-03 20:41:00 -0300

    fixing evaluation script

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,7 +1,6 @@
 
 MODEL=$1
 INPUT_NAME=$2
-METRICS=$3
 DATASET=tiagoblima/qg_squad_v1_pt
 
-python src/eval.py -m $MODEL -i $INPUT_NAME  -d $DATASET --metric_list ["sacrebleu","rouge","meteor","ter"]
+python src/eval.py -m $MODEL -i ["$INPUT_NAME"] -d $DATASET --metric_list ["sacrebleu","rouge","meteor","ter"]
