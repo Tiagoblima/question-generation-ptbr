258e101847300c80ce5587051c33284cfac15799
Tiagoblima <tiago.blima@ufrpe.br>
2024-01-04 20:03:17 -0300

    accepting multiple inputs

--- src/eval.py
+++ src/eval.py
@@ -9,49 +9,58 @@ import numpy as np
 
 @click.command()
 @click.option("-m", "model_name", type=str)
-@click.option("-d", "dataset_name", type=str)
+@click.option("-d", "dataset_name", type=str, default="tiagoblima/qg_squad_v1_pt")
 @click.option("--metrics", type=str, default="sacrebleu")
-@click.option("-i","--input_name", type=str, default="paragraph_answer")
+@click.option("-i","--input_names", type=str, default="answer,paragraph")
 @click.option("-t","--target_name", type=str, default="question")
 @click.option("--split_name", type=str, default="validation")
 @click.option("-bs", "--batch_size", type=int, default=16)
+@click.option("-ml", "--max_new_tokens", type=int, default=96)
+@click.option("--num_beams", type=int, default=5)
 @click.option("--bs_model_type", type=str, default='neuralmind/bert-base-portuguese-cased')
 def main(model_name,
          dataset_name,
          metrics, 
-         input_name,
+         input_names,
          target_name,
          split_name,
          batch_size,
+         max_new_tokens,
+         num_beams,
          bs_model_type
          ):
-
+    input_names = input_names.split(",")
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name).to(device)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    eval_ds = dts.load_dataset(dataset_name, split=split_name)
+    eval_ds = dts.load_dataset("tiagoblima/qg_squad_v1_pt", split=split_name)
 
-    def predict(batch):
-        
-        text_inputs = batch[input_name]
-        
+    def generate_input(tup_example):
+          
+            return f"{tokenizer.eos_token}".join(tup_example)
 
+    def predict(*text_inputs):
+        
+        text_inputs = [generate_input(example) for example in zip(*text_inputs)]
+        
         model_inputs = tokenizer(text_inputs,
                                   max_length=model.config.max_length, 
                                   padding=True, truncation=True, return_tensors="pt")
         # Tokenize targets with text_target=...
         for inps in model_inputs:
             model_inputs[inps] =  model_inputs[inps].to(device)
-        outputs_ids = model.generate(**model_inputs, num_beams=5,
-         max_new_tokens=96)
+        outputs_ids = model.generate(**model_inputs, num_beams=num_beams, max_new_tokens=max_new_tokens)
 
         batch.update({
             "predicted": tokenizer.batch_decode(outputs_ids, skip_special_tokens=True)
         })
         return batch
 
-    predict_ds = eval_ds.map(predict, batch_size=batch_size, batched= batch_size > 1)
+    predict_ds = eval_ds.map(predict,
+                            input_columns=input_names,
+                            batch_size=batch_size,
+                            batched= batch_size > 1)
 
     hypothesis = np.array(predict_ds["predicted"])
     references = np.expand_dims(np.array(predict_ds[target_name]), axis=1)
