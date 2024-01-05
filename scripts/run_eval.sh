91dc9614b2974b677a85d7c341e7a35c19f49274
Tiagoblima <tiago.blima@ufrpe.br>
2024-01-04 21:20:50 -0300

    solving minor issues, adding out dir

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -3,4 +3,7 @@ MODEL=$1
 INPUT_NAME=$2
 DATASET=tiagoblima/qg_squad_v1_pt
 
-python src/eval.py -m $MODEL -i $INPUT_NAME -d $DATASET --metrics "sacrebleu,rouge,meteor,ter,bert_score"
+python src/eval.py -m $MODEL \
+                   -i $INPUT_NAME  \
+                   -d $DATASET \
+                   --metrics "sacrebleu,rouge,meteor,ter,bert_score"
