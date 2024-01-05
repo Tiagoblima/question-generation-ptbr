00982e578abb6fcee470d9ad96f5ca2225d9897d
Tiagoblima <tiago.blima@ufrpe.br>
2024-01-04 21:32:14 -0300

    solving minor issues, bertscore name

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,9 +1,9 @@
-
 MODEL=$1
 INPUT_NAME=$2
 DATASET=tiagoblima/qg_squad_v1_pt
-
+OUTPUT_DIR="/content/drive/MyDrive/QuestionGeneration/Reports/"
 python src/eval.py -m $MODEL \
                    -i $INPUT_NAME  \
                    -d $DATASET \
+                   -o $OUTPUT_DIR \
                    --metrics "sacrebleu,rouge,meteor,ter,bertscore"
