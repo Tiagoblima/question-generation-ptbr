16b9187a5a7f03eb5926a78ec1cd1f4dc3f8820e
Tiagoblima <tiago.blima@ufrpe.br>
2024-01-17 07:19:34 -0300

    rm unnecessary files

--- src/run_seq2seq_qg.py
+++ src/run_seq2seq_qg.py
@@ -751,7 +751,6 @@ def main():
         args=training_args,
         train_dataset=train_dataset if training_args.do_train else None,
         eval_dataset=eval_dataset if training_args.do_eval else None,
-        eval_examples=eval_examples if training_args.do_eval else None,
         tokenizer=tokenizer,
         data_collator=data_collator,
         compute_metrics=compute_metrics if training_args.predict_with_generate else None,
