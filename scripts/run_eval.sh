27a533b8cc04c8855b9b23d391c75da74df0c1a5
Tiagoblima <tiago.blima02@gmail.com>
2024-01-27 11:05:47 -0300

    fixing eval dataset

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -1,6 +1,6 @@
 INPUTS=$1
 PRED_FILE=$2
-DATASET=tiagoblima/du-qg-squadv1_pt
+DATASET=tiagoblima/preprocessed-du-qg-squadv1_pt
 OUTPUT_DIR="/content/drive/MyDrive/QuestionGeneration/Reports/"
 python src/eval.py --pred_file $PRED_FILE  \
                    -d $DATASET \
