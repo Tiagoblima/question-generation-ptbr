603c94e89a61ef689b5216b02ecdd00ad6f69c84
Tiagoblima <tiago.blima02@gmail.com>
2024-02-03 19:12:52 -0300

    updating script

--- scripts/run_setup.sh
+++ scripts/run_setup.sh
@@ -1,13 +1,12 @@
 WRDIR="question-generation-ptbr"
-WANDB_API_KEY=""
-HF_TOKEN=""
+
+export WANDB_API_KEY=""
+export HF_TOKEN=""
+export WANDB_PROJECT="question-generation-ptbr"
 cd $WRDIR
 pip install --quiet -r requirements.txt
 git checkout exps
 git pull origin exps
 
-echo   "export WANDB_API_KEY=$WANDB_API_KEY"  > run_setup.sh
-echo   "export HF_TOKEN=$HF_TOKEN" >> run_setup.sh
-echo   "export WANDB_PROJECT=question-generation-ptbr \n" >> run_setup.sh
-echo   "source scripts/run_train.sh experiments/answer-paragraph/t5_base-qg-ap-nopeft.json" >> run_setup.sh
-echo   "source scripts/run_eval.sh tiagoblima/t5_base-qg-ap-nopeft paragraph,answer" >> run_setup.sh
+source scripts/run_train.sh experiments/answer-paragraph/t5_base-qg-ap-nopeft.json
+source scripts/run_eval.sh
